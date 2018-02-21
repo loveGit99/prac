ce49abb6d99b0df5ef4edab4cc13b35b9cafbf87
Allen shen <dreamAngel_v@163.com>
2018-02-21 23:29:45 +0800

    no message

---
+++ htxk/src/main/java/com/hhly/common/services/RegisterCenterApplication.java
@@ -0,0 +1,19 @@
+package com.hhly.common.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterCenterApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(RegisterCenterApplication.class)
+				.web(true)
+				.run(args);
+
+
+	}
+}
