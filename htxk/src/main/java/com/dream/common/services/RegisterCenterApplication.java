865cd53b1bcaba789df26543bf375accba2a9ed7
Allen shen <dreamAngel_v@163.com>
2018-02-23 22:17:56 +0800

    Refactor

--- htxk/src/main/java/com/dream/common/services/RegisterCenterApplication.java
+++ htxk/src/main/java/com/dream/common/services/RegisterCenterApplication.java
@@ -1,7 +1,6 @@
 
-package com.hhly.common.services;
+package com.dream.common.services;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
